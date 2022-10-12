912ebc6798fb2d187ef08bc8cdb627f10b5869bc
Alka Mishra <alka71807@gmail.com>
2022-10-12 15:32:31 -0700

    added port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express= require("express");
 const app=express();
 app.use(express.json());
+const port = 5000;
 
-
-app.listen(5000, ()=>{
-    console.log("App running on port ", 5000)
+app.listen(port, ()=>{
+    console.log("App running on port ", port)
 })
